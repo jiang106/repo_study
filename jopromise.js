5e4f42b3f7c3158c78c2903bbe5c296334aa66fe
Jiang <jiangzhaojing@gmail.com>
2020-06-21 12:10:35 -0400

    add some codes

--- jopromise.js
+++ jopromise.js
@@ -7,3 +7,22 @@ img1.addEventListener('load', function() {
 img1.addEventListener('error', function() {
   // argh everything's broken
 });
+
+
+
+var img1 = document.querySelector('.img-1');
+
+function loaded() {
+  // woo yey image loaded
+}
+
+if (img1.complete) {
+  loaded();
+}
+else {
+  img1.addEventListener('load', loaded);
+}
+
+img1.addEventListener('error', function() {
+  // argh everything's broken
+});
