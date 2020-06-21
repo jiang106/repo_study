251f978df530f786332173c87c3f30c4eb7c2247
Jiang <jiangzhaojing@gmail.com>
2020-06-21 15:11:52 -0400

    delete promise

--- jopromise.js
+++ jopromise.js
@@ -28,25 +28,28 @@ img1.addEventListener('error', function() {
 });
 
 
-//promise
-
-var promise = new Promise(function(resolve, reject) {
-  // do a thing, possibly async, then…
-
-  if (/* everything turned out fine */) {
-    resolve("Stuff worked!");
-  }
-  else {
-    reject(Error("It broke"));
-  }
-});
 
 
-promise.then(function(result) {
-  console.log(result); // "Stuff worked!"
-}, function(err) {
-  console.log(err); // Error: "It broke"
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // new function with promise
