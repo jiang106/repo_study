c89e0b73bb70eb6492a0bab8d563a765540d9871
Jiang <jiangzhaojing@gmail.com>
2020-06-22 23:11:53 -0400

    Revert "add sequelize basics"
    This reverts commit 4a46a3c0236cc57da82e8f10048db3c87d38076e.

--- josequelize.js
+++ josequelize.js
@@ -15,25 +15,3 @@ User.init({
   });
   console.log(jane.toJSON());
 })();
-
-// model basics
-
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
-
-const User = sequelize.define('User', {
-  // Model attributes are defined here
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING
-    // allowNull defaults to true
-  }
-}, {
-  // Other model options go here
-});
-
-// `sequelize.define` also returns the model
-console.log(User === sequelize.models.User); // true
